4d69c5df2f8735fc1984f8e550b36bb6289278ce
Tamás Benke <tombenke@gmail.com>
2020-02-05 19:38:54 +0100

    Add SendMessage() Add NowAsUnixWithPrecision()

--- nats.go
+++ nats.go
@@ -88,3 +88,34 @@ func CheckFatal(err error) {
         panic(err)
     }
 }
+
+func SendMessage(nc *nats.Conn, subject string, msg []byte) error {
+    subj := subject
+
+    nc.Publish(subj, msg)
+    nc.Flush()
+
+    if err := nc.LastError(); err != nil {
+        log.Fatal(err)
+    } else {
+        log.Printf("Published [%s] : '%s'\n", subj, msg)
+    }
+    return nil
+}
+
+// Move to axon-go-common
+func NowAsUnixWithPrecision(precision string) int64 {
+    nowNs := time.Now().UnixNano()
+    switch precision {
+    case "ns":
+        return nowNs
+    case "u", "us":
+        return nowNs / 1e3
+    case "ms":
+        return nowNs / 1e6
+    case "s":
+        return nowNs / 1e9
+    }
+    return nowNs
+}
+
